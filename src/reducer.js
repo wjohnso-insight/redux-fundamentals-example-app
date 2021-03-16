959183a9e91bc2fdfa1ada42f4080e1ec05d230e
Will <will.johnston@insight.com>
2021-03-16 10:54:19 -0400

    combineReducers
    		- Added In-Flight docs link to Readme
    		- **`reducer.js`**
    
    			- Refactored to `rootReducer`
    
    		- Installed `redux` core library
    
    	`npm insall redux` ** **
    
    		- Refactored `reducers.js` to use [combineReducers()](craftdocs://open?blockId=86F1E12A-4176-45E7-AA6B-89B726FE6344&spaceId=35b7910a-02c9-b6ae-7bc0-106a5eab9e46)

--- src/reducer.js
+++ src/reducer.js
@@ -1,52 +1,17 @@
 //TODO: [√] Add 'dummy' todo data to initial state
 //TODO: [√] Define root reducer
-//TODO: [] Define 'todos/todoAdded' reducer
+//TODO: [√] Refactor to `rootReducer`
+//TODO: [] Refactor to `combineReducers` implementation to reduce redundancy or `rootReducer` returning slices
 
-const initialState = {}
+import todosReducer from './features/todos/todosSlice'
+import filtersReducer from './features/filters/filtersSlice'
+import { combineReducers } from 'redux'
 
-export default function appReducer(state = initialState, action){ //ES6 default argument syntax for setting initial state
-    switch(action.type){
-        // case 'todos/todoAdded':{
-        //     return{
-        //         //* Copy all existing state data
-        //         ...state,
-        //         //* Update the `todos` field 
-        //         todos: [
-        //             //* spread in all old `todo` objects
-        //             ...state.todos,
-        //             //* add the new object
-        //             {
-        //                 id: nextTodoId(state.todos),
-        //                 text: action.payload, //* Returned from caller
-        //                 completed: false
-        //             }
-        //         ]
-        //     }
-        // }
-        // case 'todos/todoToggled':{
-        //     return{
-        //         ...state,
-        //         todos: state.todos.map(todo => {
-        //             if (todo.id !== action.payload){
-        //                 return todo //* this is our target todo, as determined by the action called at component level
-        //             }
-        //             return{
-        //                 ...todo, //*Spread in the rest of target todo 
-        //                 completed: !todo.completed //* updated target todo
-        //             }
-        //         })
-        //     }
-        // }
-        // case 'filters/statusFilterChanged':{
-        //     return {
-        //         ...state,
-        //         filters:{
-        //             ...state.filters,
-        //             status: action.payload //* Since this is basically an enum, its easy to return directly from dispatch caller
-        //         }
-        //     }
-        // }
-        default:
-            return state
-    }
-}
+const rootReducer = combineReducers({
+    //* Define a top-level state field named `todos`, handled by `todosReducer`
+    todos: todosReducer,
+    //* Same for filters
+    filters: filtersReducer
+})
+
+export default rootReducer
