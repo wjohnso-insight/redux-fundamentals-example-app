81851bd91ff07de4fea3cfec8386911f015913f6
Will <will.johnston@insight.com>
2021-03-17 13:20:38 -0400

    Addint the DevTools to the Store
        - Installed `redux-devtools-extension`
        - Updated `store` using `composedWithDevTools()` from
        `redux-devtools-extension`

--- src/exampleAddons/middleware.js
+++ src/exampleAddons/middleware.js
@@ -12,3 +12,11 @@ export const print3 = (storeAPI) => (next) => (action) => {
   console.log('3')
   return next(action)
 }
+
+const loggerMiddleware = storeAPI => next => action => {
+  console.log('dispatching', action)
+  let result = next(action) //* passing action to next middleware or `store.dispatch()`
+  //! as the `next()` runs, eventually the state tree is updated. Once this happens, `next()` returns
+  console.log('next state', storeAPI.getState()) 
+  return result //* Returns the return value from the next middleware or `store.dispatch`
+}
--- src/store.js
+++ src/store.js
@@ -1,12 +1,15 @@
 import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import rootReducer from './reducer'
 import { print1, print2, print3 } from './exampleAddons/middleware'
 
-const middlewareEnhancer = applyMiddleware(print1, print2, print3)
+const composedEnhancer = composeWithDevTools(
+    applyMiddleware(print1, print2, print3)
+)
 
 //* `print1...3` are middleware that log their respective number to the console between each `dispatch()` call 
 
-const store = createStore(rootReducer, middlewareEnhancer)
+const store = createStore(rootReducer, composedEnhancer)
 /*
     * `createStore(reducer, preloadedState, enhancer)` can also be described with
     * `createStore(reducer, enhancer)` since all stores don't have preloaded state
